2342ec94d41049f9268fdc369e71e28031d2cd80
Felix Dietze <felix@beerleader.net>
2016-06-21 01:36:44 +0200

    put luminance and chroma into state

--- src/main/scala/ColorAreaView.scala
+++ src/main/scala/ColorAreaView.scala
@@ -20,25 +20,28 @@ object ColorAreaView {
   val colorBorder = 2.0
   val chromaCircleRadius = width * 0.4
   val chromaCircleBorder = 2.0
-  val dragTolerance = 3
 
   case class Props(proxy: ModelProxy[RootModel]) {
-    def luminance = proxy.value.colorArea.luminance
-    def chroma = proxy.value.colorArea.chroma
     def palette = proxy.value.palette
-    def zoom = chromaCircleRadius / chroma
-
-    def insideColor(index: Int, x: Double, y: Double): Boolean = {
-      val c = palette(index)
-      val cx = c.a * zoom + width / 2
-      val cy = c.b * zoom + height / 2
-      // val mx = e.clientX - e.srcElement.getBoundingClientRect.left
-      // val my = e.clientY - e.srcElement.getBoundingClientRect.top
-      (x - cx) * (x - cx) + (y - cy) * (y - cy) <= (colorRadius + colorBorder / 2.0) * (colorRadius + colorBorder / 2.0)
-    }
   }
 
-  class Backend($: BackendScope[Props, Unit]) {
+  case class State(chroma: Double = 100, luminance: Double = 70) {
+    def zoom = chromaCircleRadius / chroma
+  }
+
+  def insideColor(p: Props, s: State, index: Int, x: Double, y: Double): Boolean = {
+    val c = p.palette(index)
+    val cx = c.a * s.zoom + width / 2
+    val cy = c.b * s.zoom + height / 2
+    val r = colorRadius + colorBorder / 2.0
+    (x - cx) * (x - cx) + (y - cy) * (y - cy) <= r * r
+  }
+
+  class Backend($: BackendScope[Props, State]) {
+
+    val canvasBgRef = Ref[raw.HTMLCanvasElement]("canvas-bg")
+    val canvasFgRef = Ref[raw.HTMLCanvasElement]("canvas-fg")
+
     var draggingLuminance = false
     var draggingPalette: Option[(LAB, Int)] = None
     var dragStartX: Double = 0
@@ -46,162 +49,157 @@ object ColorAreaView {
     var dragOffsetX: Double = 0
     var dragOffsetY: Double = 0
 
-    def initCanvas(p: Props) = Callback {
-      val canvas = document.getElementById("color-area-canvas-fg").asInstanceOf[html.Canvas]
+    def initCanvas(p: Props, s: State) = Callback {
+      for (canvas <- canvasFgRef($)) {
+        // val canvas = document.getElementById("color-area-canvas-fg").asInstanceOf[html.Canvas]
 
-      canvas.onmousewheel = (e: WheelEvent) => {
-        val mx = e.clientX - e.srcElement.getBoundingClientRect.left
-        val my = e.clientY - e.srcElement.getBoundingClientRect.top
+        canvas.onmousewheel = (e: WheelEvent) => {
+          val mx = e.clientX - e.srcElement.getBoundingClientRect.left
+          val my = e.clientY - e.srcElement.getBoundingClientRect.top
 
-        (0 until p.palette.size).reverse.find(i => p.insideColor(i, mx, my)) match {
-          case Some(i) =>
-            val col = p.palette(i)
-            p.proxy.dispatch(UpdateColor(i, col.copy(l = (col.l - e.deltaY / 10.0).max(0).min(100)))).runNow()
-          case None =>
-            p.proxy.dispatch(UpdateChroma((p.chroma + e.deltaY / 10.0).max(0).min(128))).runNow()
+          (0 until p.palette.size).reverse.find(i => insideColor(p, s, i, mx, my)) match {
+            case Some(i) =>
+              val col = p.palette(i)
+              p.proxy.dispatch(UpdateColor(i, col.copy(l = (col.l - e.deltaY / 10.0).max(0).min(100)))).runNow()
+            case None =>
+              $.modState(_.copy(chroma = (s.chroma + e.deltaY / 10.0).max(0).min(128))).runNow()
+          }
+          e.stopImmediatePropagation()
+          false
         }
-        e.stopImmediatePropagation()
-        false
-      }
 
-      //TODO: native drag event
-      canvas.onmousedown = (e: MouseEvent) => {
-        val mx = e.clientX - e.srcElement.getBoundingClientRect.left
-        val my = e.clientY - e.srcElement.getBoundingClientRect.top
+        //TODO: native drag event
+        canvas.onmousedown = (e: MouseEvent) => {
+          val mx = e.clientX - e.srcElement.getBoundingClientRect.left
+          val my = e.clientY - e.srcElement.getBoundingClientRect.top
 
-        (0 until p.palette.size).reverse.find(i => p.insideColor(i, mx, my)) match {
-          case Some(i) =>
-            val col = p.palette(i)
-            dragStartX = mx
-            dragStartY = my
-            dragOffsetX = (p.zoom) * col.a - mx
-            dragOffsetY = (p.zoom) * col.b - my
-            draggingPalette = Some((col, i))
-          case None =>
-            draggingLuminance = true
+          (0 until p.palette.size).reverse.find(i => insideColor(p, s, i, mx, my)) match {
+            case Some(i) =>
+              val col = p.palette(i)
+              dragStartX = mx
+              dragStartY = my
+              dragOffsetX = (s.zoom) * col.a - mx
+              dragOffsetY = (s.zoom) * col.b - my
+              draggingPalette = Some((col, i))
+            case None =>
+              draggingLuminance = true
+          }
+          e.stopImmediatePropagation()
         }
-        e.stopImmediatePropagation()
-      }
 
-      canvas.onmousemove = (e: MouseEvent) => {
-        val mx = e.clientX - e.srcElement.getBoundingClientRect().left
-        val my = e.clientY - e.srcElement.getBoundingClientRect().top
+        canvas.onmousemove = (e: MouseEvent) => {
+          val mx = e.clientX - e.srcElement.getBoundingClientRect().left
+          val my = e.clientY - e.srcElement.getBoundingClientRect().top
 
-        draggingPalette match {
-          case Some((col, i)) =>
-            val newCol = col.copy(
-              a = (mx + dragOffsetX) / p.zoom,
-              b = (my + dragOffsetY) / p.zoom
-            )
-            draggingPalette = Some((newCol, i))
-            p.proxy.dispatch(UpdateColor(i, col)).runNow()
-            drawForeground(p)
-
-          case None if draggingLuminance =>
-            val luminance = ((Math.PI * 2.5 + Math.atan2(my - height / 2.0, mx - width / 2.0)) / (Math.PI * 2) * 100.0) % 100.0
-            p.proxy.dispatch(UpdateLuminance(luminance)).runNow()
-
-          case _ =>
-        }
-        e.stopImmediatePropagation()
-      }
-
-      canvas.onmouseup = (e: MouseEvent) => {
-        val mx = e.clientX - e.srcElement.getBoundingClientRect().left
-        val my = e.clientY - e.srcElement.getBoundingClientRect().top
-
-        draggingPalette match {
-          case Some((col, i)) =>
-            if (dragStartX == mx && dragStartY == my) {
-              p.proxy.dispatch(UpdateLuminance(col.luminance)).runNow()
-              p.proxy.dispatch(UpdateChroma(col.chroma)).runNow()
-            } else
+          draggingPalette match {
+            case Some((col, i)) =>
+              val newCol = col.copy(
+                a = (mx + dragOffsetX) / s.zoom,
+                b = (my + dragOffsetY) / s.zoom
+              )
+              draggingPalette = Some((newCol, i))
               p.proxy.dispatch(UpdateColor(i, col)).runNow()
+              drawForeground(p, s)
 
-          case None if draggingLuminance =>
-            val luminance = ((Math.PI * 2.5 + Math.atan2(my - height / 2.0, mx - width / 2.0)) / (Math.PI * 2) * 100.0) % 100.0
-            p.proxy.dispatch(UpdateLuminance(luminance)).runNow()
+            case None if draggingLuminance =>
+              val luminance = ((Math.PI * 2.5 + Math.atan2(my - height / 2.0, mx - width / 2.0)) / (Math.PI * 2) * 100.0) % 100.0
+              $.modState(_.copy(luminance = luminance)).runNow()
 
-          case _ =>
+            case _ =>
+          }
+          e.stopImmediatePropagation()
         }
-        draggingPalette = None
-        draggingLuminance = false
-        e.stopImmediatePropagation()
-      }
 
-      drawBackground(p)
-      drawForeground(p)
+        canvas.onmouseup = (e: MouseEvent) => {
+          val mx = e.clientX - e.srcElement.getBoundingClientRect().left
+          val my = e.clientY - e.srcElement.getBoundingClientRect().top
+
+          draggingPalette match {
+            case Some((col, i)) =>
+              if (dragStartX == mx && dragStartY == my) {
+                $.modState(_.copy(luminance = col.luminance, chroma = col.chroma)).runNow()
+              } else
+                p.proxy.dispatch(UpdateColor(i, col)).runNow()
+
+            case None if draggingLuminance =>
+              val luminance = ((Math.PI * 2.5 + Math.atan2(my - height / 2.0, mx - width / 2.0)) / (Math.PI * 2) * 100.0) % 100.0
+              $.modState(_.copy(luminance = luminance)).runNow()
+
+            case _ =>
+          }
+          draggingPalette = None
+          draggingLuminance = false
+          e.stopImmediatePropagation()
+        }
+
+        drawBackground(p, s)
+        drawForeground(p, s)
+      }
     }
 
-    def drawBackground(p: Props) {
+    def drawBackground(p: Props, s: State) {
       import CanvasHelpers._
-      val canvas = document.getElementById("color-area-canvas-bg").asInstanceOf[html.Canvas]
+      for (canvas <- canvasBgRef($)) {
 
-      val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
-      val imageData = ctx.createImageData(canvas.width, canvas.height)
+        val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+        val imageData = ctx.createImageData(canvas.width, canvas.height)
 
-      val data = imageData.data.asInstanceOf[js.Array[Int]]
-      val start = System.currentTimeMillis
-      for (x <- 0 until canvas.width; y <- 0 until canvas.height) {
-        val a = (x - width / 2) / p.zoom
-        val b = (y - height / 2) / p.zoom
-        val i = (y * canvas.width + x) * 4
-        val color = ColorConversion.labToRGB(p.luminance, a.toInt, b.toInt)
-        data(i + 0) = color(0).toInt.max(0).min(255)
-        data(i + 1) = color(1).toInt.max(0).min(255)
-        data(i + 2) = color(2).toInt.max(0).min(255)
-        data(i + 3) = 255
+        val data = imageData.data.asInstanceOf[js.Array[Int]]
+        val start = System.currentTimeMillis
+        for (x <- 0 until canvas.width; y <- 0 until canvas.height) {
+          val a = (x - width / 2) / s.zoom
+          val b = (y - height / 2) / s.zoom
+          val i = (y * canvas.width + x) * 4
+          val color = ColorConversion.labToRGB(s.luminance, a.toInt, b.toInt)
+          data(i + 0) = color(0).toInt.max(0).min(255)
+          data(i + 1) = color(1).toInt.max(0).min(255)
+          data(i + 2) = color(2).toInt.max(0).min(255)
+          data(i + 3) = 255
+        }
+        val duration = System.currentTimeMillis - start
+        println(s"${duration}ms")
+        ctx.putImageData(imageData, 0, 0)
       }
-      val duration = System.currentTimeMillis - start
-      println(s"${duration}ms")
-      ctx.putImageData(imageData, 0, 0)
     }
 
-    def drawForeground(p: Props) {
+    def drawForeground(p: Props, s: State) {
       import CanvasHelpers._
-      val canvas = document.getElementById("color-area-canvas-fg").asInstanceOf[html.Canvas]
+      for (canvas <- canvasFgRef($)) {
 
-      val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+        val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
 
-      ctx.asInstanceOf[js.Dynamic].resetTransform() //TODO: add to scalajs.dom library
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.asInstanceOf[js.Dynamic].resetTransform() //TODO: add to scalajs.dom library
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-      ctx.translate(width / 2, height / 2)
+        ctx.translate(width / 2, height / 2)
 
-      def paletteCircle(ctx: CanvasRenderingContext2D, color: LAB, zoom: Double = p.zoom) {
-        percentCirle(ctx, color.a, color.b,
-          r = colorRadius / zoom,
-          width = colorBorder / zoom,
-          a = color.luminance / 100.0,
-          refa = Some(p.luminance / 100.0),
-          fillColor = Some(color.toCSS))
-      }
-
-      // circle on p.chroma:
-      percentCirle(ctx, 0, 0, chromaCircleRadius, width = chromaCircleBorder, p.luminance / 100.0)
-
-      val colors = draggingPalette match {
-        case Some((col, i)) => p.palette.updated(i, col)
-        case None => p.palette
-      }
-      if (p.chroma > 0) {
-        ctx.scale(p.zoom, p.zoom)
-        for (color <- colors) {
-          paletteCircle(ctx, color)
+        def paletteCircle(ctx: CanvasRenderingContext2D, color: LAB, zoom: Double = s.zoom) {
+          percentCirle(ctx, color.a, color.b,
+            r = colorRadius / zoom,
+            width = colorBorder / zoom,
+            a = color.luminance / 100.0,
+            refa = Some(s.luminance / 100.0),
+            fillColor = Some(color.toCSS))
         }
-      } else { // chroma is zero => infinite zoom, only draw gray colors
-        for (color <- colors if color.isGray) {
-          paletteCircle(ctx, color, zoom = 1.0)
+
+        // circle on s.chroma:
+        percentCirle(ctx, 0, 0, chromaCircleRadius, width = chromaCircleBorder, s.luminance / 100.0)
+
+        val colors = draggingPalette match {
+          case Some((col, i)) => p.palette.updated(i, col)
+          case None => p.palette
+        }
+        if (s.chroma > 0) {
+          ctx.scale(s.zoom, s.zoom)
+          for (color <- colors) {
+            paletteCircle(ctx, color)
+          }
+        } else { // chroma is zero => infinite zoom, only draw gray colors
+          for (color <- colors if color.isGray) {
+            paletteCircle(ctx, color, zoom = 1.0)
+          }
         }
       }
-    }
-
-    def setLuminance(p: Props)(e: ReactEventI) = {
-      p.proxy.dispatch(UpdateLuminance(Try(e.target.value.toDouble).getOrElse(0.0)))
-    }
-
-    def setChroma(p: Props)(e: ReactEventI) = {
-      p.proxy.dispatch(UpdateChroma(Try(e.target.value.toDouble).getOrElse(0.0)))
     }
 
     val widthAttr = "width".reactAttr
@@ -209,17 +207,17 @@ object ColorAreaView {
     def render(p: Props) = {
       <.div(
         <.div(
-          ^.width := width,
-          ^.height := height,
+          ^.width := s"${width}px",
+          ^.height := s"${height}px",
           ^.position := "relative",
           <.canvas(
-            ^.id := "color-area-canvas-bg",
+            ^.ref := "canvas-bg",
             widthAttr := width,
             heightAttr := height,
             ^.position := "absolute"
           ),
           <.canvas(
-            ^.id := "color-area-canvas-fg",
+            ^.ref := "canvas-fg",
             widthAttr := width,
             heightAttr := height,
             ^.position := "absolute",
@@ -227,8 +225,8 @@ object ColorAreaView {
           )
         )
       // <.br,
-      // <.input(^.`type` := "range", ^.value := p.chroma, ^.min := 0, ^.max := 128, ^.step := 1, ^.onChange ==> setChroma(p)),
-      // <.input(^.value := p.chroma, ^.onChange ==> setChroma(p), ^.size := 3),
+      // <.input(^.`type` := "range", ^.value := s.chroma, ^.min := 0, ^.max := 128, ^.step := 1, ^.onChange ==> setChroma(p)),
+      // <.input(^.value := s.chroma, ^.onChange ==> setChroma(p), ^.size := 3),
       // <.br,
       // <.input(^.`type` := "range", ^.width := width, ^.value := p.luminance, ^.min := 0, ^.max := 100, ^.step := 1, ^.onChange ==> setLuminance(p))
       )
@@ -236,9 +234,10 @@ object ColorAreaView {
   }
 
   private val component = ReactComponentB[Props]("ColorAreaView")
+    .initialState(State())
     .renderBackend[Backend]
-    .componentDidMount(c => c.backend.initCanvas(c.props))
-    .componentDidUpdate(c => c.$.backend.initCanvas(c.currentProps))
+    .componentDidMount(c => c.backend.initCanvas(c.props, c.state))
+    .componentDidUpdate(c => c.$.backend.initCanvas(c.currentProps, c.currentState))
     .build
 
   def apply(proxy: ModelProxy[RootModel]) = component(Props(proxy))
--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -4,32 +4,22 @@ import diode._
 import diode.react._
 
 // Model
-case class RootModel(colorArea: ColorArea, palette: IndexedSeq[LAB] = IndexedSeq.empty)
-case class ColorArea(luminance: Double, chroma: Double)
+case class RootModel(palette: IndexedSeq[LAB] = IndexedSeq.empty)
 
 // Actions
-case class UpdateLuminance(luminance: Double)
-case class UpdateChroma(chroma: Double)
 case class AddColor(color: LAB)
 case class UpdateColor(index: Int, color: LAB)
 
 // Circuit
 object AppCircuit extends Circuit[RootModel] with ReactConnector[RootModel] {
   // define initial value for the application model
-  def initialModel = RootModel(ColorArea(70, 100), Array(
+  def initialModel = RootModel(Array(
     LAB(30, -75, 20),
     LAB(60, 15, 12),
     LAB(70, 100, 0),
     LAB(30, 0, 0),
     LAB(50, 1, 0)
   ))
-
-  val colorAreaHandler = new ActionHandler(zoomRW(_.colorArea)((m, v) => m.copy(colorArea = v))) {
-    override def handle = {
-      case UpdateLuminance(l) => updated(value.copy(luminance = l))
-      case UpdateChroma(l) => updated(value.copy(chroma = l))
-    }
-  }
 
   val paletteHandler = new ActionHandler(zoomRW(_.palette)((m, v) => m.copy(palette = v))) {
     override def handle = {
@@ -38,5 +28,5 @@ object AppCircuit extends Circuit[RootModel] with ReactConnector[RootModel] {
     }
   }
 
-  override val actionHandler = composeHandlers(colorAreaHandler, paletteHandler)
+  override val actionHandler = composeHandlers(paletteHandler)
 }
