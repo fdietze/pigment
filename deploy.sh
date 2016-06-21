01ba806ee33e1fc57e0b887d345a79b032962f26
Felix Dietze <felix@beerleader.net>
2016-06-21 20:41:12 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 NAME="pigment"
 
-# sbt fullOptJS
+sbt fullOptJS
 
 mkdir -p deploy
 cp $NAME.html deploy
 cp target/scala-2.11/$NAME-{jsdeps.min.js,opt.js,launcher.js} deploy
 
-# ipfs add -r deploy
-# ipfs name publish /ipfs/$DIRHASH $PUBKEY
+DIRHASH=$(ipfs add -r deploy | tail -1 | cut -d " " -f2)
+ipfs name publish /ipfs/$DIRHASH
