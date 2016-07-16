70a4eedec923f85552e7df0fcb1ec31e19ea3efc
Felix Dietze <felix@beerleader.net>
2016-07-16 04:45:01 +0200

    add color to new group

--- src/main/scala/PaletteView.scala
+++ src/main/scala/PaletteView.scala
@@ -24,60 +24,63 @@ object PaletteView {
   class Backend($: BackendScope[Props, Unit]) {
 
     def render(p: Props) = {
-      <.table(
-        <.thead(<.tr(<.th())),
-        <.tbody(
-          p.groups.map {
-            case (groupId, group) =>
-              val avgLuminance = group.map(_._1.luminance).sum / group.size
-              val avgChroma = group.map(_._1.chroma).sum / group.size
-              val hues = group.map(_._1.hue)
-              val huesOver = hues :+ (hues.min + 2 * PI)
-              val freeHue = if (huesOver.size == 0)
-                50
-              else if (huesOver.size < 2)
-                huesOver.head + PI
-              else
-                huesOver.sorted.sliding(2).maxBy { case Seq(a, b) => b - a }.sum / 2
+      <.div(
+        <.button(^.onClick --> p.proxy.dispatch(AddColor(Color(LCH(50, 50, 50), p.groups.keys.max + 1))), "+"),
+        <.table(
+          <.thead(<.tr(<.th())),
+          <.tbody(
+            p.groups.map {
+              case (groupId, group) =>
+                val avgLuminance = group.map(_._1.luminance).sum / group.size
+                val avgChroma = group.map(_._1.chroma).sum / group.size
+                val hues = group.map(_._1.hue)
+                val huesOver = hues :+ (hues.min + 2 * PI)
+                val freeHue = if (huesOver.size == 0)
+                  50
+                else if (huesOver.size < 2)
+                  huesOver.head + PI
+                else
+                  huesOver.sorted.sliding(2).maxBy { case Seq(a, b) => b - a }.sum / 2
 
-              <.tr(
-                group.map {
-                  case (col, i) =>
-                    val rgb = col.lab.toRGB
-                    <.td(
-                      ^.textAlign := "center",
-                      <.div(^.width := "50px", ^.height := "50px",
-                        ^.backgroundColor := col.toCSS),
-                      <.pre(
-                        ^.fontFamily := "monospace",
-                        ^.fontSize := "8px",
-                        ^.margin := "0px",
-                        "%3d,%3d,%3d" format (col.l.toInt, col.a.toInt, col.b.toInt)
-                      ),
-                      <.pre(
-                        ^.fontFamily := "monospace",
-                        ^.fontSize := "8px",
-                        ^.margin := "0px",
-                        s"#${rgb.toHEX}"
-                      ),
-                      <.pre(
-                        ^.fontFamily := "monospace",
-                        ^.fontSize := "8px",
-                        ^.margin := "0px",
-                        "%3d,%3d,%3d" format (rgb.r, rgb.g, rgb.b)
-                      ),
-                      <.button(
-                        ^.fontSize := "8px",
-                        ^.onClick --> p.proxy.dispatch(RemoveColor(i)), "remove"
+                <.tr(
+                  group.map {
+                    case (col, i) =>
+                      val rgb = col.lab.toRGB
+                      <.td(
+                        ^.textAlign := "center",
+                        <.div(^.width := "50px", ^.height := "50px",
+                          ^.backgroundColor := col.toCSS),
+                        <.pre(
+                          ^.fontFamily := "monospace",
+                          ^.fontSize := "8px",
+                          ^.margin := "0px",
+                          "%3d,%3d,%3d" format (col.l.toInt, col.a.toInt, col.b.toInt)
+                        ),
+                        <.pre(
+                          ^.fontFamily := "monospace",
+                          ^.fontSize := "8px",
+                          ^.margin := "0px",
+                          s"#${rgb.toHEX}"
+                        ),
+                        <.pre(
+                          ^.fontFamily := "monospace",
+                          ^.fontSize := "8px",
+                          ^.margin := "0px",
+                          "%3d,%3d,%3d" format (rgb.r, rgb.g, rgb.b)
+                        ),
+                        <.button(
+                          ^.fontSize := "8px",
+                          ^.onClick --> p.proxy.dispatch(RemoveColor(i)), "remove"
+                        )
                       )
-                    )
-                },
-                <.td(
-                  ^.verticalAlign := "top",
-                  <.button(^.onClick --> p.proxy.dispatch(AddColor(Color(LCH(avgLuminance, avgChroma, freeHue), groupId))), "+")
+                  },
+                  <.td(
+                    ^.verticalAlign := "top",
+                    <.button(^.onClick --> p.proxy.dispatch(AddColor(Color(LCH(avgLuminance, avgChroma, freeHue), groupId))), "+")
+                  )
                 )
-              )
-          }
+            }
+          )
         )
       )
     }
