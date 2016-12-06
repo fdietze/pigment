1ac9f84cdbb9f7a7ea4d64232f850badb9d58a3f
Felix Dietze <felix@beerleader.net>
2016-12-06 16:51:00 +0100

    move boopickle-base64 code to Export.scala

---
+++ src/main/scala/Export.scala
@@ -0,0 +1,37 @@
+package pigment
+
+package object export {
+  def toBase64(model: RootModel): String = {
+    import scala.scalajs.js
+    import org.scalajs.dom._
+    import js.typedarray._
+    import js.typedarray.TypedArrayBufferOps._
+    import js.Dynamic.global
+    import java.nio.ByteBuffer
+    import boopickle.Default._
+
+    implicit class ByteBufferOpt(data: ByteBuffer) {
+      def toArrayBuffer: ArrayBuffer = {
+        if (data.hasTypedArray()) {
+          // get relevant part of the underlying typed array
+          data.typedArray().subarray(data.position, data.limit).buffer
+        } else {
+          // fall back to copying the data
+          val tempBuffer = ByteBuffer.allocateDirect(data.remaining)
+          val origPosition = data.position
+          tempBuffer.put(data)
+          data.position(origPosition)
+          tempBuffer.typedArray().buffer
+        }
+      }
+    }
+
+    val byteBuffer = Pickle.intoBytes(model)
+    val buffer = new Uint8Array(byteBuffer.toArrayBuffer, 0, byteBuffer.remaining)
+    val s = new StringBuilder(buffer.length)
+    for (i <- 0 until buffer.length) {
+      s ++= global.String.fromCharCode(buffer(i)).asInstanceOf[String]
+    }
+    window.btoa(s.result)
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,37 +12,9 @@ import diode.react._
 import vectory._
 
 class SaveProcessor extends ActionProcessor[RootModel] {
-  import boopickle.Default._
-  import js.typedarray._
-  import js.typedarray.TypedArrayBufferOps._
-  import js.Dynamic.global
-  import java.nio.ByteBuffer
-
-  implicit class ByteBufferOpt(data: ByteBuffer) {
-    def toArrayBuffer: ArrayBuffer = {
-      if (data.hasTypedArray()) {
-        // get relevant part of the underlying typed array
-        data.typedArray().subarray(data.position, data.limit).buffer
-      } else {
-        // fall back to copying the data
-        val tempBuffer = ByteBuffer.allocateDirect(data.remaining)
-        val origPosition = data.position
-        tempBuffer.put(data)
-        data.position(origPosition)
-        tempBuffer.typedArray().buffer
-      }
-    }
-  }
-
   def process(dispatch: diode.Dispatcher, action: Any, next: Any => diode.ActionResult[RootModel], currentModel: RootModel): ActionResult[RootModel] = {
-    val byteBuffer = Pickle.intoBytes(currentModel)
-    val buffer = new Uint8Array(byteBuffer.toArrayBuffer, 0, byteBuffer.remaining)
-    val s = new StringBuilder(buffer.length)
-    for (i <- 0 until buffer.length) {
-      s ++= global.String.fromCharCode(buffer(i)).asInstanceOf[String]
-    }
-    val encoded = window.btoa(s.result)
-    window.location.hash = s"#${encoded}"
+    val encoded = export.toBase64(currentModel)
+    window.location.hash = s"#$encoded"
 
     // call the next processor
     next(action)
